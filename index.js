6ae8a6814578843d85247d443a2b6341c027ff9f
racheldboese <94402323+racheldboese@users.noreply.github.com>
2022-01-23 21:42:20 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function UATSpaceTrans(){
+    location.replace("uatspacepage.html")
+}
+function indexTrans(){
+    location.replace("index.html")
+}
+
+function objTrans(){
+    location.replace("BOARDOBJECTIVES.HTML")
+}
